1dd02c393fe3e35138b68b01c16c8ff8c2f15c90
Madiha Amjad <madihaamjad06@gmail.com>
2024-01-12 09:53:37 +0500

    initial commit

---
+++ solidty_practice.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.4 <0.9.0;
+
+
+contract prac{
+
+string str;
+
+function st(string memory ab) public pure returns(string memory){
+
+return ab;
+}
+
+
+}
