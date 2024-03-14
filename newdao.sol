4ee3b2242dbe0de96db88cbd7ab631d3528a41d2
DAPP <119337228+MadihaAmjad@users.noreply.github.com>
2024-03-14 08:37:16 +0000

    Add files via upload

---
+++ newdao.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DAO{
+uint public Pid;
+    uint public Mid;
+    uint public MemberShipFee = 5 ether;
+    address immutable  owner;
+    // uint public WinningPercent = (70* 100)/Mid;
+
+
+}
